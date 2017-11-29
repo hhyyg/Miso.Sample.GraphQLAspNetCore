0a7b9bb5f8d50d44dcba5c917be2139fcb69ec35
hiroka.85 <hiroka.yago@gmail.com>
2017-11-29 18:20:25 +0900

    プロジェクト ファイルを追加します。

---
+++ GraphQLSampleWeb/Controllers/GraphQLController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GraphQL.Types;
+using GraphQL;
+using GraphQLSampleWeb.GraphQL;
+
+namespace GraphQLSampleWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("graphql")]
+    public class GraphQLController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            var schema = Schema.For(@"
+    type Query {
+        user: Person
+    }
+
+    type Person {
+        id: String
+        name: String
+    }
+");
+            var root = new { User = new { Id = "1", Name = "Taro" } };
+
+            var result = await new DocumentExecuter().ExecuteAsync(_ =>
+            {
+                _.Schema = schema;
+                _.Query = query.Query;
+                _.Root = root;
+            }).ConfigureAwait(false);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ GraphQLSampleWeb/webpack.config.js
@@ -0,0 +1,36 @@
+﻿const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const output = "wwwroot";
+
+module.exports = {
+    context: path.resolve(__dirname),
+    entry: {
+        'bundle': './Scripts/app.js'
+    },
+    output: {
+        path: path.join(__dirname, output),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.json']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            { test: /\.css$/, loaders: ['css'] }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './node_modules/graphiql/graphiql.css', to: 'graphiql.css' },
+            { from: './index.html', to: 'index.html' }
+        ])
+    ]
+}
